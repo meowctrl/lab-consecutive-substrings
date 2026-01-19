7a36db37948ba58bdb79fb38c24bdc74dc014530
carol <carolineallen1012@gmail.com>
2026-01-19 18:46:47 -0500

    commit

--- consecutive_substrings.js
+++ consecutive_substrings.js
@@ -1,6 +1,13 @@
 function consecutiveSubstrings(string) {
-  // type your code here
-}
+  const substrings = [];
+
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i; j < string.length; j++) {
+      substrings.push(string.slice(i, j + 1));
+    };
+  };
+  return substrings;
+};
 
 if (require.main === module) {
   // add your own tests in here
@@ -12,6 +19,9 @@ if (require.main === module) {
   console.log("Expecting: ['a']");
   console.log("=>", consecutiveSubstrings('a'));
 }
+
+  console.log("Expecting: ['p','ph','pho','phon','phone','h','ho','hon','hone','o','on','one','n','ne','e']");
+  console.log("=>", consecutiveSubstrings('phone'));
 
 module.exports = consecutiveSubstrings;
 
